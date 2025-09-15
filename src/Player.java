e4255b4bad74ae8333216150311b34e7473c0650
alexjons987 <alexander.jonsson987@outlook.com>
2025-09-15 12:28:53 +0200

    Add Player class

---
+++ src/Player.java
@@ -0,0 +1,133 @@
+import java.util.ArrayList;
+
+public class Player {
+
+    public enum PlayerRace {
+
+    }
+
+    private Stats stats;
+    private int health;
+    private ArrayList<Item> inventory;
+
+    // Equipped armor
+    Armor equippedHead;
+    Armor equippedChest;
+    Armor equippedLegs;
+    Armor equippedFeet;
+    Armor equippedNecklace;
+    Armor equippedRingLeft;
+    Armor equippedRingRight;
+
+    // Equipped weapon(s)
+    Weapon equippedMainHand;
+    Weapon equippedOffHand;
+
+    // Altars
+    // Active buffs
+
+    public Player() {
+        this.stats = new Stats();
+        this.health = this.stats.getMaxHP();
+        this.equippedMainHand = null;
+        this.equippedOffHand = null;
+    }
+
+    public Player(int strength, int vigor, int agility, int dexterity, int will, int knowledge, int resourcefulness) {
+        this.stats = new Stats(strength, vigor, agility, dexterity, will, knowledge, resourcefulness);
+        this.health = this.stats.getMaxHP();
+        this.equippedHead = null;
+        this.equippedChest = null;
+        this.equippedLegs = null;
+        this.equippedFeet = null;
+        this.equippedNecklace = null;
+        this.equippedRingLeft = null;
+        this.equippedRingRight = null;
+        this.equippedMainHand = null;
+        this.equippedOffHand = null;
+    }
+
+    public Stats getStats() {
+        return this.stats;
+    }
+
+    public int getHealth() {
+        return this.health;
+    }
+
+    public float getHealthPercentage() {
+        return (float) this.health / this.stats.getMaxHP();
+    }
+
+    public boolean equipArmor(Armor armor) {
+        return true;
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(String.format(
+            "Strength: %d%n" + //
+            "Vigor: %d%n" + //
+            "Agility: %d%n" + //
+            "Dexterity: %d%n" + //
+            "Will: %d%n" + //
+            "Knowledge: %d%n" + //
+            "Resourcefulness: %d%n" + //
+            "Health: %d/%d (%.1f%%)%n" + //
+            "Action Speed: %.1f%%",
+            this.stats.getStrength(),
+            this.stats.getVigor(),
+            this.stats.getAgility(),
+            this.stats.getDexterity(),
+            this.stats.getWill(),
+            this.stats.getKnowledge(),
+            this.stats.getResourcefulness(),
+            this.health,
+            this.stats.getMaxHP(),
+            this.getHealthPercentage() * 100,
+            this.stats.getActionSpeed()
+        ));
+        return sb.toString();
+    }
+
+    // Ability
+    // Second Wind (Campfire to refresh) - When your health drops below 30%, gain +40% lifesteal for your 12 next attacks.
+    // War Cry (Campfire to refresh) - When your health drops below 50%, gain +25% max health bonus for the rest of the combat encounter (current health scales accordingly).
+    // Divine Protection (Pray to refresh) - At the start of the combat encounter, receive the blessing of the divine and gain +30% physical damage reduction for the 6 next incoming attacks.
+
+    // Perks
+    // Adrenaline Spike - When your health goes below 40%, gain +15% action speed for your 12 next attacks.
+    // Combo Attack - For every other attack, gain +10% damage bonus.
+    // Defense Master - Gain an additional +10% armor from equipped armor.
+    // Sword Mastery - While using a Sword-type weapon, gain +2 weapon damage and 5% action speed.
+    // Axe Specialization - While using an Axe-type weapon, gain +3 weapon damage.
+    // Two-Hander - While using a two-handed (2H) weapon, gain +5% damage bonus.
+    // Executioner - While using an Axe-type weapon, gain +5% critical chance bonus.
+    // Blunt Weapon Mastery - While using a Mace-type weapon, gain +10% damage bonus.
+    // Jokester - Grant yourself +2 to all Attributes.
+    // Poisoned Daggers  - While using a Dagger-type weapon, deal an additional 4 magic damage.
+    // Ambush - When entering a combat encounter, gain +100% Action Speed and +25% physical damage bonus for your first attack.'
+    // Dagger Mastery - While using a Dagger-type weapon, gain +2 weapon damage.
+    // Traps and Locks - You can unlock locked chests without lockpicks.
+    // Fermata - Gain +5 Resourcefulness.
+    // Rapier Mastery - When a rapier is equipped, gain +2 weapon damage and 5% action speed.
+    // Story Teller - Gain +3 Will and +3 Knowledge.
+
+    // Attacking
+    // Each weapon has a swing timer (affected by player action speed)
+    // Critical hits +100% damage bonus
+    // On miss, increase time of next attack
+    // Miss - 10%: Increase time of next attack by +100%.
+    // Crit - 5%: Deal +100% weapon damage.
+    // Hit - 85%: Deal weapon damage.
+
+    // Looting chests
+    // Create 1 of each item -> randomly pick one -> randomize rarity
+    // POOR 15%
+    // UNCOMMON 30%
+    // RARE 25%
+    // EPIC 15%
+    // LEGENDARY 10%
+    // UNIQUE 4%
+    // ARTIFACT 1%
+}
